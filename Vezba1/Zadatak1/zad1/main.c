559bf286ef3bd195c2c640f9aa80787b09c6e840
AleksandarEE168-2018 <aleksandaraleksic11@gmail.com>
2021-05-09 09:46:33 +0200

    Initial code added

--- Vezba1/Zadatak1/zad1/main.c
+++ Vezba1/Zadatak1/zad1/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
+    int a = 5;
+    int b = 4;
+    int c;
+       if(a > b)
+          c = a - b;
+       else
+          c = b - a;
+          printf("Result: %d\n", c);
     return 0;
 }
