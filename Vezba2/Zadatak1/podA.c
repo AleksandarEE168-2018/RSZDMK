78ea22de0b7b814aa86d4eee7d2179bb41941408
AleksandarEE168-2018 <aleksandaraleksic11@gmail.com>
2021-05-09 13:49:58 +0200

    LED Blink with Timer 0

--- Vezba2/Zadatak1/podA.c
+++ Vezba2/Zadatak1/podA.c
@@ -3,17 +3,45 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-int main(void)
+unsigned char slika_led = 0xFF;
+unsigned char t0_cnt = 0;
+ISR(TIMER0_COMPA_vect)
 {
-   DDRD = 0xff; //port D -> izlaz
-   DDRB |= 1 << 4; //PB4 -> izlaz
-   PORTB &= ~(1 << 4); //PB4 = 0, cime se ukljucuje tranzistor Q0
+ //prekid tajmera 0 usled dostizanja vrednosti registra OCR0A
+ t0_cnt++;
+}
+int main()
+{
+
+ DDRD = 0xff; //port D -> izlaz
+ DDRB |= 1 << 4; //PB4 -> izlaz
+ PORTB &= ~(1 << 4); //PB4 = 0, cime se ukljucuje tranzistor Q0
+
+ //inicijalizacija tajmera 0:
+ TCCR0A = 0x02; //tajmer 0: CTC mod
+#ifdef POD_A
+ TCCR0B = 0x03; //tajmer 0: fclk = fosc/64
+ OCR0A = 123; //perioda tajmera 0: 124 Tclk (OCR0A + 1 = 124)
+#else
+   TCCR0B = 0x04; //tajmer 0: fclk = fosc/256
+   OCR0A = 249; //perioda tajmera 0: 250 Tclk (OCR0A + 1 = 250)
+#endif
+   TIMSK0 = 0x02; //dozvola prekida tajmera 0
+   //usled dostizanja vrednosti registra OCR0A
+
+ sei(); //I = 1 (dozvola prekida)
+
  while(1)
  {
-    PORTD = 0xfe;
-    _delay_ms(500);
-    PORTD = 0xff;
-    _delay_ms(500);
+   #ifdef POD_A
+   if (t0_cnt){
+   #else
+   if (t0_cnt == 125){
+   #endif
+      t0_cnt = 0;
+      slika_led ^= 0x01;
+      PORTD = slika_led;
+ }
  }
  return 0;
 }
